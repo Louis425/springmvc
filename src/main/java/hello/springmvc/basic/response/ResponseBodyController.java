21d2690e4d0be45c8f89285df7ba670ee0f35312
Louis425 <ehdrjsdl2661@gmail.com>
2024-03-06 23:26:18 +0900

    spring

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -35,6 +35,7 @@ public class ResponseBodyController {
         return "ok";
     }
 
+
     @GetMapping("/response-body-json-v1")
     public ResponseEntity<HelloData> responseBodyJsonV1() {
         HelloData helloData = new HelloData();
